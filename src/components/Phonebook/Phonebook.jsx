c6fed2d7c83b210e86bdf4d5e31e9aa192ce5d08
Ihor-Samburenko <samburenko.igor@gmail.com>
2023-02-28 13:24:08 +0200

    fix localstarage

--- src/components/Phonebook/Phonebook.jsx
+++ src/components/Phonebook/Phonebook.jsx
@@ -10,10 +10,10 @@ import { nanoid } from 'nanoid';
 class Phonebook extends Component {
   state = {
     contacts: [
-      // { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      // { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      // { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      // { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
     ],
 
     filter: '',
@@ -28,10 +28,11 @@ class Phonebook extends Component {
     }
   }
 
-  componentDidUpdate() {
+  componentDidUpdate(prevProps, prevState) {
     const { contacts } = this.state;
-
-    localStorage.setItem('phonebook', JSON.stringify(contacts));
+    if (contacts.length !== prevState.contacts.length) {
+      localStorage.setItem('phonebook', JSON.stringify(contacts));
+    }
   }
 
   onDelete = id => {
